10508a6cb1e919fd7971fd655d12b708b5529a33
Idriss Elbaqri <ielbaqri15@gmail.com>
2025-10-07 11:50:02 +0200

    Back to top

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,11 +25,14 @@ const toggleNavbar = function(){
 addEventOnElem(navTogglers, "click", toggleNavbar);
 
 const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
 const activeElemOnScroll = function(){
     if(window.scrollY > 100){
         header.classList.add("active");
+        backTopBtn.classList.add("active");
     } else{
         header.classList.remove("active");
+        backTopBtn.classList.remove("active");
     }
 }
 
